45cf30f340f3d1c1f99e0632d89b592d5c3a18ba
alexandreramosdev <alexandresantos84@gmail.com>
2025-12-04 12:35:25 -0300

    Criado e conectado rotas na db

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import fastify from "fastify";
 
 import { courses } from "./src/database/schema.ts";
 import { db } from "./src/database/client.ts";
+import { eq } from "drizzle-orm";
 
 const server = fastify({
   logger: {
@@ -15,16 +16,63 @@ const server = fastify({
   },
 });
 
-server.get("/courses", async (request, reply) => {
+server.post("/api/courses", async (request, reply) => {
+  type Body = {
+    name: string;
+    description?: string;
+  };
+
+  const { name, description } = request.body as Body;
+
+  if (!name) {
+    return reply.status(400).send({ message: "Name is required" });
+  }
+
   const result = await db
-    .select({ id: courses.id, name: courses.name })
-    .from(courses);
+    .insert(courses)
+    .values({ name, description })
+    .returning();
+
+  return reply.status(201).send({ courseId: result[0].id });
+});
+
+server.get("/api/courses", async (request, reply) => {
+  const result = await db.select().from(courses);
 
   return reply.status(200).send({ courses: result });
 });
 
-server.get("/health", async () => {
-  return { status: "ok" };
+server.get("/api/courses/:id", async (request, reply) => {
+  type Params = {
+    id: string;
+  };
+
+  const params = request.params as Params;
+  const courseId = params.id;
+
+  const result = await db
+    .select()
+    .from(courses)
+    .where(eq(courses.id, courseId));
+
+  if (result.length === 0) {
+    return reply.status(404).send({ message: "Course not found" });
+  }
+
+  return reply.status(200).send({ course: result[0] });
+});
+
+server.delete("/api/courses/:id", async (request, reply) => {
+  type Params = {
+    id: string;
+  };
+
+  const params = request.params as Params;
+  const courseId = params.id;
+
+  await db.delete(courses).where(eq(courses.id, courseId));
+
+  return reply.status(200).send({ message: "Course deleted successfully" });
 });
 
 server.listen({ port: 3000 }).then(() => {
