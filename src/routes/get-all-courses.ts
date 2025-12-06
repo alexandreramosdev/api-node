1f4fb7bb7c32bc612b3384723a424b001f041ffd
alexandreramosdev <alexandresantos84@gmail.com>
2025-12-06 07:27:41 -0300

    Add search, ordering, and pagination to getAllCoursesRoute
    - Added support for searching courses by title
    - Added support for ordering courses by title
    - Added support for pagination with a default page size of 4
    - Updated the response schema to include the total number of courses

--- src/routes/get-all-courses.ts
+++ src/routes/get-all-courses.ts
@@ -2,6 +2,7 @@ import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { courses } from "../database/schema.ts";
 import { db } from "../database/client.ts";
 import z from "zod";
+import { and, asc, ilike, SQL } from "drizzle-orm";
 
 export const getAllCoursesRoute: FastifyPluginAsyncZod = async (server) => {
   server.get(
@@ -10,6 +11,11 @@ export const getAllCoursesRoute: FastifyPluginAsyncZod = async (server) => {
       schema: {
         tags: ["Courses"],
         summary: "Get all courses",
+        querystring: z.object({
+          search: z.string().optional(),
+          orderBy: z.enum(["title"]).optional().default("title"),
+          page: z.coerce.number().optional().default(1),
+        }),
         response: {
           200: z.object({
             courses: z.array(
@@ -19,14 +25,32 @@ export const getAllCoursesRoute: FastifyPluginAsyncZod = async (server) => {
                 description: z.string().nullable(),
               })
             ),
+            total: z.number(),
           }),
         },
       },
     },
     async (request, reply) => {
-      const result = await db.select().from(courses);
+      const { search, orderBy, page } = request.query;
 
-      return reply.status(200).send({ courses: result });
+      const conditions: SQL[] = [];
+
+      if (search) {
+        conditions.push(ilike(courses.title, `%${search}%`));
+      }
+
+      const [result, total] = await Promise.all([
+        db
+          .select()
+          .from(courses)
+          .orderBy(asc(courses[orderBy]))
+          .offset((page - 1) * 4)
+          .limit(4)
+          .where(and(...conditions)),
+        db.$count(courses, and(...conditions)),
+      ]);
+
+      return reply.status(200).send({ courses: result, total });
     }
   );
 };
